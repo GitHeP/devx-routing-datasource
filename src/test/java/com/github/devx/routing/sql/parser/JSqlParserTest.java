6b3da4f3088a93da87ca4a80b352f3d2af6d9dc1
hepeng <anonymou_s@163.com>
2023-05-30 22:37:31 +0800

    test parse sub select

--- src/test/java/com/github/devx/routing/sql/parser/JSqlParserTest.java
+++ src/test/java/com/github/devx/routing/sql/parser/JSqlParserTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +26,7 @@ class JSqlParserTest {
     }
 
     @Test
-    void testParse_1() {
+    void testParseJoin() {
 
         String sql = "SELECT customers.customer_name, orders.order_id, order_details.product_name, order_details.unit_price, " +
                 "order_details.quantity, (order_details.unit_price * order_details.quantity) AS total_price\n" +
@@ -47,5 +48,30 @@ class JSqlParserTest {
         assertThat(statement.getJoinTables()).containsAll(Arrays.asList("orders" , "order_details"));
         assertThat(statement.getSubTables()).isEmpty();
         assertThat(statement.getDatabases()).isEmpty();
+    }
+
+    @Test
+    void testParseSubSelect() {
+
+        String sql = "SELECT s1.student_name, s1.student_id, s1.grade\n" +
+                "FROM students s1\n" +
+                "INNER JOIN (\n" +
+                "    SELECT AVG(grade) AS avg_grade\n" +
+                "    FROM students\n" +
+                "    WHERE grade >= 60\n" +
+                ") s2\n" +
+                "ON s1.grade >= s2.avg_grade\n" +
+                "WHERE s1.gender = 'F'\n" +
+                "ORDER BY s1.grade DESC;\n";
+
+        SqlStatement statement = sqlParser.parse(sql);
+        assertThat(statement).isNotNull();
+        assertThat(statement.isWrite()).isFalse();
+        assertThat(statement.isRead()).isTrue();
+        assertThat(statement.getFromTable()).isEqualTo("students");
+        assertThat(statement.getJoinTables()).isEmpty();
+        assertThat(statement.getSubTables()).contains("students");
+        assertThat(statement.getDatabases()).isEmpty();
+
     }
 }
