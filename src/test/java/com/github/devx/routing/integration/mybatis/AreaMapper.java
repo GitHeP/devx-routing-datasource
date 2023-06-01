98d365f3c175df8790ce43dc4c0606b3b73676bf
hepeng <anonymou_s@163.com>
2023-06-01 22:38:11 +0800

    more mybatis testing

--- src/test/java/com/github/devx/routing/integration/mybatis/AreaMapper.java
+++ src/test/java/com/github/devx/routing/integration/mybatis/AreaMapper.java
@@ -1,6 +1,10 @@
 package com.github.devx.routing.integration.mybatis;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 
 /**
@@ -10,4 +14,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AreaMapper {
+
+    @Insert("insert into area (id , name) values (#{val.id} , #{val.name})")
+    int insert(@Param("val") Map<String , Object> val);
+
+    @Insert("update area set name = #{val.name} where id = #{val.id}")
+    int updateById(@Param("val") Map<String , Object> val);
+
+    @Insert("delete from area where id = #{val.id}")
+    int deleteById(Long id);
+
+    Map<String , Object> selectById(Long id);
 }
--- src/test/java/com/github/devx/routing/integration/mybatis/EmployeeMapper.java
+++ src/test/java/com/github/devx/routing/integration/mybatis/EmployeeMapper.java
@@ -1,6 +1,10 @@
 package com.github.devx.routing.integration.mybatis;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 /**
  * @author Peng He
@@ -9,4 +13,15 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface EmployeeMapper {
+
+    @Insert("insert into employee (id , name , department_id) values (#{val.id} , #{val.name} , #{val.departmentId})")
+    int insert(@Param("val") Map<String , Object> val);
+
+    @Insert("update employee set name = #{val.name} , department_id = #{val.departmentId} where id = #{val.id}")
+    int updateById(@Param("val") Map<String , Object> val);
+
+    @Insert("delete from employee where id = #{val.id}")
+    int deleteById(Long id);
+
+    Map<String , Object> selectById(Long id);
 }
