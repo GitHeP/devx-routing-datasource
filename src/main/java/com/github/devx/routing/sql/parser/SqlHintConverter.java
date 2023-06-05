37b110ef38814476f8c910cc86320d56b3558896
hepeng <anonymou_s@163.com>
2023-06-05 10:59:42 +0800

    comment

--- src/main/java/com/github/devx/routing/sql/parser/SqlHintConverter.java
+++ src/main/java/com/github/devx/routing/sql/parser/SqlHintConverter.java
@@ -1,10 +1,16 @@
 package com.github.devx.routing.sql.parser;
 
 /**
+ * Convert SQL hint key-value configuration to the type specified by generics.
  * @author Peng He
  * @since 1.0
  */
 public interface SqlHintConverter<T> {
 
+    /**
+     * extract and convert SQL hint
+     * @param hint {@link SqlHint}
+     * @return generics type T instance , maybe null
+     */
     T convert(SqlHint hint);
 }
