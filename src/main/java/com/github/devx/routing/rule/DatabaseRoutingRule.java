343a3ee9b368179f958f5db27f374532cbdc1580
hepeng <anonymou_s@163.com>
2023-06-08 22:43:35 +0800

    is options size equal 1

--- src/main/java/com/github/devx/routing/rule/DatabaseRoutingRule.java
+++ src/main/java/com/github/devx/routing/rule/DatabaseRoutingRule.java
@@ -44,7 +44,7 @@ public class DatabaseRoutingRule implements SqlAttributeRoutingRule {
 
     @Override
     public int priority() {
-        return Integer.MAX_VALUE - 500;
+        return Integer.MAX_VALUE - 100;
     }
 
     @Override
